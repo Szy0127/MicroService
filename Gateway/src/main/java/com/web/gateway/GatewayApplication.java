d899cc903c0e956950012d71c25d0bc34c5e66ad
Szy0127 <1799853523@qq.com>
2022-11-09 15:45:02 +0800

    feat: gateway

---
+++ Gateway/src/main/java/com/web/gateway/GatewayApplication.java
@@ -0,0 +1,28 @@
+package com.web.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+//				.route(p->p.path("/author/**")
+//						.filters(f->f.rewritePath("/author",""))
+//						.uri("http://localhost:8081"))
+				.route(p->p.path("/**")
+						.uri("http://localhost:8081")
+				)
+				.build();
+	}
+
+}
